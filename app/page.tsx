466393a91e6f1cb0e8a60bbda22a96291eccf2c0
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-06 21:03:56 +0000

    feat: working on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screeb flex-col items-center justify-center p-4 bg-gray-900 texxt-white">
       <div className="max-w-3xl text-center">
-        <h1></h1>
+        <h1>RantPal</h1>
       </div>
     </main>
   );
