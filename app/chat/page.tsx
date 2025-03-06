d9b1c4eadbda6d6ff92bf2341a4fed8c0b0f36f7
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-06 21:25:04 +0000

    feat:working on the chat page

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,8 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='flex flex-col h-screen bg-gray-900'>
+        {/* Header */}
+        <header className = "bg-gray-800 p-4 text-white flex justify-between  items-center">
+        <Link href="/">RantPal</Link>
+        </header>
+    </div>
   )
 }
 
