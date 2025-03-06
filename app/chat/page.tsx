e2461abd9d1e33b5a225607818b7f6bf2c348a83
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-06 22:09:52 +0000

    feat:chatpage/ working on chat messages area

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -99,7 +99,7 @@ const page = () => {
             <div className="flex justify-start">
                 <div className="bg-gray-700 text-white rounded-lg p-4 max-w-[80%]">
                     <div className="flex space-x-2">
-                        <div className="w-2"></div>
+                        <div className="w-2 h-2 bg-gray-500 rounded-full animate-bounce"></div>
                     </div>
                 </div>
             </div>
