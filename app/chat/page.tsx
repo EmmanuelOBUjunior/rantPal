41dd2daa6251abeb9cd555ce0f3c850f6cc0ac05
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-06 21:27:09 +0000

    feat:working on the chat page

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -7,6 +7,7 @@ const page = () => {
         {/* Header */}
         <header className = "bg-gray-800 p-4 text-white flex justify-between  items-center">
         <Link href="/" className="text-xl font-bold text-purple-400">RantPal</Link>
+        <div className= "text-sm bg-purple-600 px-3 py-1 rounded-full">Therapy Mode</div>
         </header>
     </div>
   )
