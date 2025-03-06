5e8d5d05fce7f1648cb71f8b95eed1f52d62c38c
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-06 21:26:01 +0000

    feat:working on the chat page

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
     <div className='flex flex-col h-screen bg-gray-900'>
         {/* Header */}
         <header className = "bg-gray-800 p-4 text-white flex justify-between  items-center">
-        <Link href="/">RantPal</Link>
+        <Link href="/" className="text-xl font-bold">RantPal</Link>
         </header>
     </div>
   )
