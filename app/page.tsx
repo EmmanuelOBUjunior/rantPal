2845fd3f5e4cbec85a21f155ef27d88564eb9cca
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-06 21:02:26 +0000

    feat: working on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className = "flex min-h-screeb flex-col items-center justify-center p-4 bg-gray-900 texxt-white">
-      Hello there
+      <div className="max-w-3xl text-center">
+
+      </div>
     </main>
   );
 }
