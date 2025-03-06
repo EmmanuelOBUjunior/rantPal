43510978f144e0d5e9632c8c43b10d0038d290ae
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-06 22:02:31 +0000

    feat:chatpage/ working on chat messages area

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -87,7 +87,9 @@ const page = () => {
 
       {/* Chat Messages */}
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
-        
+        {messages.map((message)=>(
+            <div key={message.id} className={`flex ${message.sender=== 'user'? 'justify-end' : 'justify-start'}`}></div>
+        ))}
       </div>
 
       {/* Input Area */}
