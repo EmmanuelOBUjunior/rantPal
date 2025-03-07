195ec292c798f6f2ed0d5ed32808352eb12d6509
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-07 21:22:18 +0000

    feat: working on the chat input component

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,22 +1,26 @@
-'use client'
-import { useChat } from "@/providers/ChatContext"
-import React, { useState } from "react"
+"use client";
+import { useChat } from "@/providers/ChatContext";
+import React, { useState } from "react";
 
 const ChatInput = () => {
-    const [input, setInput] = useState('second')
-    const {sendMessage, isLoading} = useChat()
+  const [input, setInput] = useState("second");
+  const { sendMessage, isLoading } = useChat();
 
-    const handleSubmit = (e:React.FormEvent)=>{
-        e.preventDefault()
-        if(!input.trim()) return;
-        sendMessage(input)
-        setInput('')
-    }
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!input.trim()) return;
+    sendMessage(input);
+    setInput("");
+  };
   return (
-    <form onSubmit={handleSubmit} className = "bg-gray-800 p-4 flex gap-2">
-        <input type="text" value={input} onChange={(e)=setInput(e.target.value)} />
+    <form onSubmit={handleSubmit} className="bg-gray-800 p-4 flex gap-2">
+      <input
+        type="text"
+        value={input}
+        onChange={(e = setInput(e.target.value))}
+      />
     </form>
-  )
-}
+  );
+};
 
-export default ChatInput
+export default ChatInput;
