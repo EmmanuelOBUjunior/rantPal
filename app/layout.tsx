cfb0d2641dd12b1fdf55258c1d4826da054e15db
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-07 19:56:04 +0000

    feat:layout - updating layout to include the chat provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ChatProvider } from "@/providers/ChatContext";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,7 +22,10 @@ export default function RootLayout({
       <body
         className={inter.className}
       >
+        <ChatProvider>
         {children}
+
+        </ChatProvider>
       </body>
     </html>
   );
