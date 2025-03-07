c1abab048bd7e1d9b6b8187226f674044f36c37d
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-07 21:25:12 +0000

    feat: working on the chat input component

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -19,6 +19,8 @@ const ChatInput = () => {
         value={input}
         onChange={(e) => setInput(e.target.value)}
         className="felx-1 bg-gray-700 text-white rounded-lg px-4 py-2 focus:outline-none focus:ring-2"
+        placeholder="Rant away.. What's bothering you today"
+        disabled={isLoading}
       />
     </form>
   );
