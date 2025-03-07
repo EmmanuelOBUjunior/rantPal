cae61ca685845ff207273b6af6ee9653a6024ee4
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-07 23:22:16 +0000

    feat:working on the toggle theme component

--- components/ToggleTheme.tsx
+++ components/ToggleTheme.tsx
@@ -1,4 +1,12 @@
+'use client'
+import { useEffect, useState } from "react"
+
 const ToggleTheme = () => {
+    const [darkMode, setDarkMode] = useState(true)
+
+    useEffect(()=>{
+        const isDarkMode = localStorage.getItem('darkMode') === 'true' || (!(darkMode in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    },[])
   return (
     <div>ToggleTheme</div>
   )
