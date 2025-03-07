8d97627aab2cd70984f6db6d7bc64c042fc1ba07
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-07 23:37:25 +0000

    feat:working on the toggle theme component

--- components/ToggleTheme.tsx
+++ components/ToggleTheme.tsx
@@ -31,7 +31,7 @@ const ToggleTheme = () => {
     <button
       onClick={toggleDarkMode}
       className="p-2 rounded-full bg-gray-700 dark:bg-gray-600 text-yellow-400 dark:text-blue-300 hover:bg-gray-600 transition-colors"
-      aria-label={darkMode ? 'Switch to light mode': 'Switch to dark mode'}
+      aria-label={darkMode ? "Switch to light mode" : "Switch to dark mode"}
     ></button>
   );
 };
