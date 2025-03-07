75e74a2f726f3384616698b803dbe21a9401aa8d
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-07 23:52:35 +0000

    feat: working on the theme context

---
+++ providers/ThemeContext.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+type ThemeContextType = {
+    darkMode: boolean,
+    toggleDarkMode: () => void
+}
+const ThemeContext = () => {
+  return (
+    <div>ThemeContext</div>
+  )
+}
+
+export default ThemeContext
