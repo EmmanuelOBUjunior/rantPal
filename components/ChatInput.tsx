9179ba240e8e9013a9affb1b904b247072257401
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-07 21:16:12 +0000

    feat: working on the chat input component

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,5 +1,12 @@
+'use client'
+import { useChat } from "@/providers/ChatContext"
+import { useState } from "react"
 
 const ChatInput = () => {
+    const [input, setInput] = useState('second')
+    const {sendMessage, isLoading} = useChat()
+
+    
   return (
     <div>ChatInput</div>
   )
