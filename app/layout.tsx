fc4913f129350cdf3c1954b1c86022af991246f2
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-08 00:08:16 +0000

    feat: working on the theme context

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ChatProvider } from "@/providers/ChatContext";
+import { ThemeProvider } from "@/providers/ThemeContext";
 
 const inter = Inter({
   subsets: ["latin"],
