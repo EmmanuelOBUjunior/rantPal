d01443945e8b46a837670c1c2d7ae5ddbcf3a7c0
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-08 00:32:22 +0000

    feat:updating homepage with theme toggle

--- components/ToggleTheme.tsx
+++ components/ToggleTheme.tsx
@@ -1,33 +1,10 @@
 "use client";
 import { useEffect, useState } from "react";
 import { SunIcon,MoonIcon } from "@heroicons/react/24/solid";
+import { useTheme } from "@/providers/ThemeContext";
 
 const ToggleTheme = () => {
-  const [darkMode, setDarkMode] = useState(true);
-
-  useEffect(() => {
-    const isDarkMode =
-      localStorage.getItem("darkMode") === "true" ||
-      (!("darkMode" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches);
-    setDarkMode(isDarkMode);
-    updateTheme(isDarkMode);
-  }, []);
-
-  const toggleDarkMode = () => {
-    const newDarkMode = !darkMode;
-    setDarkMode(newDarkMode);
-    updateTheme(newDarkMode);
-    localStorage.setItem("darkMode", String(newDarkMode));
-  };
-
-  const updateTheme = (isDark: boolean) => {
-    if (isDark) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  };
+  const {darkMode, toggleDarkMode} = useTheme() 
   return (
     <button
       onClick={toggleDarkMode}
