c0d1b52f78d0154e96811c0b073801add57f0a72
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-09 19:55:35 +0000

    feat:chatmessage.tsx - applying toggling theme

--- components/ChatMessage.tsx
+++ components/ChatMessage.tsx
@@ -7,7 +7,7 @@ type ChatMessageType={
 const ChatMessage = ({message}: ChatMessageType) => {
   return (
     <div className={`flex ${message.sender === 'user'? 'justify-end': 'justify-start'}`}>
-        <div className = {`max-w-[80%] rounded-lf p-4 ${message.sender === 'user'? 'bg-purple-600 text-white': 'bg-gray-700 text-white'}`}>
+        <div className = {`max-w-[80%] rounded-lf p-4 ${message.sender === 'user'? 'bg-purple-600 text-white dark:bg-purple-500': 'dark:bg-gray-700 bg-gray-200 text-gray-900 dark:text-white'}`}>
             {message.content}
         </div>
     </div>
