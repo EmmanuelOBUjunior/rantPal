950606784dd32d5bfd3d1cf0cc191ad139164364
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-09 19:50:19 +0000

    feat:chat page/ applying dark mode

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -35,7 +35,7 @@ const page = () => {
           </div>
           <button
             onClick={clearChat}
-            className="text-sm bg-gray-200 dark:bg-gray-300 dark:hover:bg-gray-600 px-3 py-1 rounded-full text-gray-700 dark:text-gray-300 transition-colors"
+            className="text-sm bg-gray-200 dark:bg-gray-300 dark:hover:bg-gray-600 px-3 py-1 rounded-full text-gray-700 dark:text-gray-800 transition-colors"
           >
             New Chat
           </button>
@@ -50,7 +50,7 @@ const page = () => {
 
         {isLoading && (
           <div className="flex justify-start">
-            <div className="bg-gray-700 text-white rounded-lg p-4 max-w-[80%]">
+            <div className="bg-gray-200 dark:bg-gray-700  text-gray-900 dark:text-white rounded-lg p-4 max-w-[80%]">
               <div className="flex space-x-2">
                 <div className="w-2 h-2 bg-gray-500 rounded-full animate-bounce"></div>
                 <div
