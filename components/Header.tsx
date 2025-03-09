39743eb274af1631871166e839f2f3dcb7feee59
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-09 20:17:29 +0000

    feat:working on the header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,23 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 
 type HeaderType = {
-    showNewChatButton?: boolean,
-    onNewChat?: ()=> void;
-}
+  showNewChatButton?: boolean;
+  onNewChat?: () => void;
+};
 
-const Header = ({showNewChatButton = false, onNewChat}: HeaderType) => {
+const Header = ({ showNewChatButton = false, onNewChat }: HeaderType) => {
   return (
     <header className="bg-white dark:bg-gray-800 p-4 text-gray-900 dark:text-white flex justify-between items-center shadow-sm transition-colors duration-200">
-        <Link href='/' className="text-xl font-bold text-purple-600 dark:text-purple-400">
+      <Link
+        href="/"
+        className="text-xl font-bold text-purple-600 dark:text-purple-400"
+      >
         RantPal
-        </Link>
+      </Link>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
