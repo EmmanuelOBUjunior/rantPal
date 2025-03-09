02411163df33ef14a5e528a54c57075cfbe782c9
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-09 21:28:12 +0000

    feat: header.tsx making header  sticky

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -25,7 +25,7 @@ const page = () => {
       {/* Header */}
       <Header showNewChatButton={true} onNewChat={clearChat}/>
       {/* Chat Messages */}
-      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+      <div className="flex-1 overflow-y-auto p-4 pt-6 pb-20 space-y-4">
         {messages.map((message) => (
           <ChatMessage key={message.id} message={message} />
         ))}
