2647d5e1c8c8bc5616e5879e40fcfc09c8b2ea48
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-09 20:15:24 +0000

    feat:working on the header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ type HeaderType = {
 
 const Header = ({showNewChatButton = false, onNewChat}: HeaderType) => {
   return (
-    <header className="bg-white dark:bg-gray-800 p-4 text-gray-900 dark:text-white flex justify-between intems-center shadow-sm transition-colors duration-200">
+    <header className="bg-white dark:bg-gray-800 p-4 text-gray-900 dark:text-white flex justify-between items-center shadow-sm transition-colors duration-200">
 
     </header>
   )
