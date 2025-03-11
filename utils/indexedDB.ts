21313ab8d00729f3bc3790b3c0747fd6bc447f8a
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-11 21:46:33 +0000

    feat:setting up indexedDB

--- utils/indexedDB.ts
+++ utils/indexedDB.ts
@@ -3,3 +3,11 @@
 const DB_NAME = 'rantpal-db'
 const DB_VERSION = 1
 const CHAT_STORAGE_NAME = 'chat-messages'
+
+//Initialize the database
+export const initDB = ():Promise<IDBDatabase> =>{
+return new Promise((resolve, reject))=>{
+const request = indexedDB.open(DB_NAME, DB_VERSION)
+
+}
+}
