57f0b0aa0f6b3fe14e055b86c0774a727cb8f268
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-11 21:49:25 +0000

    feat:setting up indexedDB

--- utils/indexedDB.ts
+++ utils/indexedDB.ts
@@ -6,8 +6,15 @@ const CHAT_STORAGE_NAME = 'chat-messages'
 
 //Initialize the database
 export const initDB = ():Promise<IDBDatabase> =>{
-return new Promise((resolve, reject))=>{
+return new Promise((resolve, reject)=>{
 const request = indexedDB.open(DB_NAME, DB_VERSION)
 
+request.onerror = (event)=>{
+    reject('Error opening Indexed database')
 }
+request.onsuccess = (event)=>{
+    resolve(request.result)
+}
+
+})
 }
