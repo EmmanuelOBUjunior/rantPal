67cb013339d54767f07a68e63682118c3b706486
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-03-12 19:41:32 +0000

    new: modifying ai model

--- utils/deepseekAIService.ts
+++ utils/deepseekAIService.ts
@@ -2,7 +2,13 @@ import {getRandomResponse} from './aiResponses'
 
 export const generateDeepSeekResponse = async(userMessage:string):Promise<string> =>{
     try{
-
+        const response = await fetch('https://openrouter.ai/api/v1/chat/completions',{
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.OPENROUTER_API_KEY}`
+            },
+        })
     }catch(error){
         console.error("Error generating DeepSeek response: ", error);
         return getRandomResponse(userMessage);
